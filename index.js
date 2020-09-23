a777c25606dd3f7548337824f2c29699cf292306
Deepesh Dragoneel <deepeshash444@gmail.com>
2020-09-23 13:03:03 +0530

    Fixed the glitch in the tick

--- index.js
+++ index.js
@@ -7,16 +7,24 @@ function move(){
 
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
-    secHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    if(secondsDegrees>=440){
+        secHand.classList.add("notrans");
+    }
+    secHand.style.transform = "rotate("+secondsDegrees+"deg)";
 
     const mins = now.getMinutes();
-    const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+    const minsDegrees = (mins / 60) * 360 + (seconds / 60) * 6 +90;
+    if (minsDegrees >= 448) {
+        minHand.classList.add("notrans");
+    }
     minHand.style.transform = `rotate(${minsDegrees}deg)`;
 
     const hour = now.getHours();
-    const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+    const hourDegrees = ((hour / 12) * 360)+((mins/60)*30)+90;
     hourHand.style.transform = `rotate(${hourDegrees}deg)`;
-
+    console.log(seconds + "=" +secondsDegrees);
+    console.log(mins + "=" +minsDegrees);
+    console.log(hour + "=" +hourDegrees);
 }
 
 setInterval(move, 1000);
